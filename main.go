7c007155f89f97801cab18d2846aad16a15f32f9
Stacy Drivas <18819209916@163.com>
2024-10-14 16:01:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"cc-go-canal/config"
+	"cc-go-canal/syncEs"
+	"cc-go-canal/table"
 	"fmt"
 	"log"
 	"os"
@@ -12,19 +15,16 @@ import (
 )
 
 func main() {
-
 	// 192.168.199.17 替换成你的canal server的地址
 	// example 替换成-e canal.destinations=example 你自己定义的名字
-	connector := client.NewSimpleCanalConnector("192.168.126.21", 11111,
-		"canal2", "canal123!@#QWE", "test2", 60000, 60*60*1000)
+	connector := client.NewSimpleCanalConnector(config.AppConfig.AlibabaCanal.Address, config.AppConfig.AlibabaCanal.Port,
+		config.AppConfig.AlibabaCanal.Username, config.AppConfig.AlibabaCanal.Password, config.AppConfig.AlibabaCanal.Destination, 60000, 60*60*1000)
 	err := connector.Connect()
-	fmt.Println("这里")
+	fmt.Println("启动")
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println("这里2")
-
 	// https://github.com/alibaba/canal/wiki/AdminGuide
 	//mysql 数据解析关注的表，Perl正则表达式.
 	//
@@ -39,7 +39,7 @@ func main() {
 	//  5.  多个规则组合使用：canal\\..*,mysql.test1,mysql.test2 (逗号分隔)
 
 	//err = connector.Subscribe(".*\\..*")
-	err = connector.Subscribe("blog\\..*")
+	err = connector.Subscribe(config.AppConfig.AlibabaCanal.Database + "\\..*") //数据库\\所有表
 	//err = connector.Subscribe("canal\\\\..")
 	if err != nil {
 		log.Println(err)
@@ -47,6 +47,7 @@ func main() {
 	}
 
 	fmt.Println(connector.Connected)
+	fmt.Println(connector.ClientIdentity)
 	for {
 
 		message, err := connector.Get(100, nil, nil)
@@ -55,13 +56,13 @@ func main() {
 			os.Exit(1)
 		}
 		batchId := message.Id
+
 		if batchId == -1 || len(message.Entries) <= 0 {
 			time.Sleep(300 * time.Millisecond)
-			//fmt.Println("===没有数据了=== "+ time.DateTime)
+			//fmt.Println("===没有数据了=== " + time.DateTime)
 			continue
 		}
-
-		fmt.Println("有数据*********************")
+		//处理数据
 		printEntry(message.Entries)
 
 	}
@@ -80,30 +81,99 @@ func printEntry(entrys []pbe.Entry) {
 		if rowChange != nil {
 			eventType := rowChange.GetEventType()
 			header := entry.GetHeader()
-			fmt.Println(fmt.Sprintf("================> binlog[%s : %d],name[%s,%s], eventType: %s", header.GetLogfileName(), header.GetLogfileOffset(), header.GetSchemaName(), header.GetTableName(), header.GetEventType()))
-
+			fmt.Println("")
+			fmt.Println("")
+			fmt.Println(fmt.Sprintf(" %s库.%s 表有变化", header.GetSchemaName(), header.GetTableName())) //GetSchemaName 数据库
+			_, exit := syncEs.NeedInEsTableName[header.GetTableName()]
+			if exit {
+				syncEs.CreateChatMsgIndex(header.GetSchemaName()) // 创建索引
+			}
 			for _, rowData := range rowChange.GetRowDatas() {
-				fmt.Println(rowData)
 				if eventType == pbe.EventType_DELETE {
-					printColumn(rowData.GetBeforeColumns())
+					fmt.Println("……………………………… 数据删除显示开始 ………………………………")
+					printColumn(rowData.GetBeforeColumns(), nil)
+					fmt.Println("************ 数据删除显示结束 ************")
 				} else if eventType == pbe.EventType_INSERT {
-					printColumn(rowData.GetAfterColumns())
-					printColumn(rowData.GetAfterColumns())
+					fmt.Println("……………………………… 数据新增显示开始 ………………………………")
+					rowDataRe := make(map[string]string) // 创建一个 map 用于存储结果
+					printColumn(rowData.GetAfterColumns(), rowDataRe)
+					if _, exit := syncEs.NeedInEsTableName[header.GetTableName()]; exit {
+						switch header.GetTableName() {
+						case table.TableChatMag:
+							syncEs.CreateOrUpdateChatMsgDocument(header.GetTableName(), rowDataRe)
+							break
+						}
+					}
+					fmt.Println(rowDataRe)
+					//table.CreateOrUpdateChatMsgDocument()
+					fmt.Println("************ 数据新增显示结束 ************")
 				} else {
-					fmt.Println("-------> before")
-					printColumn(rowData.GetBeforeColumns())
-					fmt.Println("-------> after")
-					printColumn(rowData.GetAfterColumns())
+					fmt.Println("……………………………… 数据更新显示开始 ………………………………")
+					// 创建一个切片
+
+					beforeData := []map[string]interface{}{}
+					for _, col := range rowData.GetBeforeColumns() {
+						beforeData = append(beforeData, map[string]interface{}{
+							"column": col.GetName(),
+							"value":  col.GetValue(),
+						})
+					}
+					afterData := []map[string]interface{}{}
+					for _, col := range rowData.GetAfterColumns() {
+						afterData = append(afterData, map[string]interface{}{
+							"column":  col.GetName(),
+							"value":   col.GetValue(),
+							"updated": col.GetUpdated(),
+						})
+					}
+					fmt.Println("旧数据")
+					//旧数据
+					for _, datum := range beforeData {
+						fmt.Print(fmt.Sprintf(" %s:%s ", datum["column"], datum["value"]))
+					}
+					fmt.Println("")
+
+					rowDataRe := make(map[string]string)
+					fmt.Println("新数据")
+					updateData := []map[string]interface{}{}
+					for i, datum := range afterData {
+						if datum["updated"] == true {
+							//变化的值
+							updateData = append(updateData, map[string]interface{}{
+								"column":   datum["column"],
+								"oldValue": beforeData[i]["value"],
+								"newValue": datum["value"],
+							})
+						}
+						rowDataRe[datum["column"].(string)] = datum["value"].(string)
+						fmt.Print(fmt.Sprintf(" %s:%s ", datum["column"], datum["value"]))
+					}
+					fmt.Println("")
+					fmt.Println("变化的数据")
+					for _, datum := range updateData {
+						fmt.Println(fmt.Sprintf("字段:%s 从:%s 改为:%s", datum["column"], datum["oldValue"], datum["newValue"]))
+					}
+					if _, exit := syncEs.NeedInEsTableName[header.GetTableName()]; exit {
+						switch header.GetTableName() {
+						case table.TableChatMag:
+							syncEs.CreateOrUpdateChatMsgDocument(header.GetTableName(), rowDataRe)
+							break
+						}
+					}
+
+					fmt.Println("……………………………… 数据更新显示结束 ………………………………")
 				}
 			}
 		}
 	}
 }
 
-func printColumn(columns []*pbe.Column) {
+func printColumn(columns []*pbe.Column, rowData map[string]string) {
 	for _, col := range columns {
-		fmt.Println(fmt.Sprintf("%s : %s  update= %t", col.GetName(), col.GetValue(), col.GetUpdated()))
+		fmt.Print(fmt.Sprintf(" %s:%s ", col.GetName(), col.GetValue()))
+		rowData[col.GetName()] = col.GetValue()
 	}
+	fmt.Println("")
 }
 
 func checkError(err error) {
